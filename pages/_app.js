6b833add16783873d9b1f45c109b838c0a6e9e52
ianujshringi <ajwarrior1999@gmail.com>
2022-07-11 15:55:27 +0530

    * Hope this will fix the error

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,33 @@ import { ThemeProvider } from "next-themes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { WalletProvider } from "../components/WalletProvider";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="dark:from-transparent dark:to-transparent bg-gradient-to-r from-slate-50 to-grey-50 dark:bg-gray-900 w-screen min-h-screen overflow-hidden">
-      <ThemeProvider attribute="class">
-        <ToastContainer theme="dark" />
-        <WalletProvider>
-          <Navbar />
-          <Component {...pageProps} />
-        </WalletProvider>
-      </ThemeProvider>
-    </div>
-  );
+  const [showing, setShowing] = useState(false);
+  useEffect(() => {
+    setShowing(true);
+  }, []);
+
+  if (!showing) {
+    return null;
+  }
+
+  if (typeof window === "undefined") {
+    return <></>;
+  } else {
+    return (
+      <div className="dark:from-transparent dark:to-transparent bg-gradient-to-r from-slate-50 to-grey-50 dark:bg-gray-900 w-screen min-h-screen overflow-hidden">
+        <ThemeProvider attribute="class">
+          <ToastContainer theme="dark" />
+          <WalletProvider>
+            <Navbar />
+            <Component {...pageProps} />
+          </WalletProvider>
+        </ThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default MyApp;
